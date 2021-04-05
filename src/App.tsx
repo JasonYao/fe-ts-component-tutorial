cafe1769053c232c130cb2a2935dfe73ae4a0e96
Jason Yao <Hello@JasonYao.com>
2021-04-05 04:43:53 -0400

    Added in routing logic, broken gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+import {Homepage} from "./Homepage/Homepage";
+import {Gallery} from "./gallery/Gallery";
+
+function AppRouter() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/gallery">Component Gallery</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Homepage} />
+          <Route path="/gallery" component={Gallery} />
+        </div>
+      </Router>
   );
 }
 
-export default App;
+export default AppRouter;
